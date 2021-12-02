8b8bb760d700678888136e37b161b8f1882a4fbe
Class Account <ee106a-aak@c105-10.local>
2021-12-02 10:09:26 -0800

    add recent changes

--- src/camera_world/src/camera_matrix.py
+++ src/camera_world/src/camera_matrix.py
@@ -5,6 +5,11 @@
 #             in RPY (radian) [3.114, 0.005, 0.160]
 #             in RPY (degree) [178.394, 0.311, 9.191]
 
+# At time 1638415335.773
+# - Translation: [0.747, 0.114, -0.270]
+# - Rotation: in Quaternion [0.999, 0.012, -0.024, 0.019]
+#             in RPY (radian) [3.104, 0.049, 0.023]
+#             in RPY (degree) [177.869, 2.785, 1.338]
 
 # # to get this 
 # roslaunch realsense2_camera/launch/rs_rgbd.launch
@@ -18,18 +23,26 @@
 #             in RPY (radian) [1.116, -0.095, -1.561]
 #             in RPY (degree) [63.935, -5.439, -89.438]
 
+# - Translation: [1.113, -0.276, -0.172]
+# - Rotation: in Quaternion [-0.175, -0.221, 0.544, 0.790]
+#             in RPY (radian) [-0.551, -0.159, 1.251]
+#             in RPY (degree) [-31.592, -9.110, 71.682]
+
+
 
 from autolab_core import RigidTransform 
 
 # T_robot_world 
-trans = np.asarray([0.551, -0.578, -0.194])
-quat = np.asarray([[0.997, 0.080, -0.002, 0.014]])
+trans = np.asarray([0.747, 0.114, -0.270])
+quat = np.asarray([[0.999, 0.012, -0.024, 0.019]])
 
 rot = RigidTransform.rotation_from_quaternion(quat)
 T_robot_world = RigidTransform(rotation=rot, translation=trans, from_frame="robot", to_frame="world")
 
 # T_camera_world 
-trans = [0.980, -0.176, 0.213]
-quat = [0.347, -0.401, -0.578, 0.620]
+trans = [1.113, -0.276, -0.172]
+quat = [-0.175, -0.221, 0.544, 0.790]
 rot = RigidTransform.rotation_from_quaternion(quat)
-T_robot_world = RigidTransform(rotation=rot, translation=trans, from_frame="camera", to_frame="world")
+T_camera_world = RigidTransform(rotation=rot, translation=trans, from_frame="camera", to_frame="world")
+
+
--- src/planning/src/path_trajectory_test.py
+++ src/planning/src/path_trajectory_test.py
@@ -1,133 +1,186 @@
 #!/usr/bin/env python
-"""
-Path Planning Script for Lab 7
-Author: Valmik Prabhu
-"""
-import sys
-assert sys.argv[1] in ("sawyer", "baxter")
-ROBOT = sys.argv[1]
 
-if ROBOT == "baxter":
-    from baxter_interface import Limb
-else:
-    from intera_interface import Limb
+# import sys
+# assert sys.argv[1] in ("sawyer", "baxter")
+# ROBOT = sys.argv[1]
 
-import rospy
-import numpy as np
-import traceback
+# if ROBOT == "baxter":
+#     from baxter_interface import Limb
+# else:
+#     from intera_interface import Limb
 
-from intera_interface import gripper as robot_gripper
+# import rospy
+# from moveit_msgs.srv import GetPositionIK, GetPositionIKRequest, GetPositionIKResponse
+# from geometry_msgs.msg import PoseStamped
+# from moveit_commander import MoveGroupCommander
+# import numpy as np
+# from numpy import linalg
+# import sys
 
-from moveit_msgs.msg import OrientationConstraint
-from moveit_msgs.msg import PositionConstraint
+# if sys.argv[1] == 'sawyer':
+#     from intera_interface import gripper as robot_gripper
+# else:
+#     from baxter_interface import gripper as robot_gripper
 
-from geometry_msgs.msg import PoseStamped
+# def main(robo):
+#     # Wait for the IK service to become available
+#     rospy.wait_for_service('compute_ik')
+#     rospy.init_node('service_query')
+#     arm = 'left'
+#     # Create the function used to call the service
+#     compute_ik = rospy.ServiceProxy('compute_ik', GetPositionIK)
+#     if robo == 'sawyer':
+#         arm = 'right'
 
-from path_planner import PathPlanner
-try:
-    from controller_pid import Controller
-except ImportError:
-    pass
+#     raw_input('Press [ Enter ]: ')
     
-def main():
-    """
-    Main Script
-    """
+#     # Construct the request
+#     request = GetPositionIKRequest()
+#     request.ik_request.group_name = arm + "_arm"
 
-    # Make sure that you've looked at and understand path_planner.py before starting
+#     # If a Sawyer does not have a gripper, replace '_gripper_tip' with '_wrist' instead
+#     link = arm + "_gripper"
+#     if robo == 'sawyer':
+#         link += '_tip'
 
-    planner = PathPlanner("right_arm")
+#     request.ik_request.ik_link_name = link
+#     request.ik_request.attempts = 20
+#     request.ik_request.pose_stamped.header.frame_id = "base"
+    
+#     # Set up the right gripper
+#     right_gripper = robot_gripper.Gripper('right')
 
-    Kp = 0.2 * np.array([0.4, 2, 1.7, 1.5, 2, 2, 3])
-    Kd = 0.01 * np.array([2, 1, 2, 0.5, 0.8, 0.8, 0.8])
-    Ki = 0.01 * np.array([1.4, 1.4, 1.4, 1, 0.6, 0.6, 0.6])
-    Kw = np.array([0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9])
+#     # Calibrate the gripper (other commands won't work unless you do this first)
+#     print('Calibrating...')
+#     right_gripper.calibrate()
+#     rospy.sleep(2.0)
 
-    gripper = robot_gripper.Gripper('right_gripper')
-    # Calibrate the gripper (other commands won't work unless you do this first)
-    print('Calibrating...')
-    gripper.calibrate()
-    rospy.sleep(2.0)
-    print('opening')
-    gripper.open()
-    rospy.sleep(1.0)
-    print('opened')
-    print('closing')
-    gripper.close()
-    rospy.sleep(1.0)
-    print('closed')
+#     # Open the right gripper
+#     print('Opening...')
+#     right_gripper.open()
+#     rospy.sleep(1.0)
+#     print('Done!')
 
-    # planner.add_box_obstacle(size, name, pose)
+#     # CV Pipeline
+#     # Should return the position of the cup in robot frame
 
-    # #Create a path constraint for the arm
-    # #UNCOMMENT FOR THE ORIENTATION CONSTRAINTS PART
-    orien_const = OrientationConstraint()
-    orien_const.link_name = "right_gripper_tip";
-    orien_const.header.frame_id = "base";
-    orien_const.orientation.x = 1.0;
-    orien_const.orientation.y = 0.0;
-    orien_const.orientation.z = 0.0;
-    orien_const.absolute_x_axis_tolerance = 0.2;
-    orien_const.absolute_y_axis_tolerance = 0.2;
-    orien_const.absolute_z_axis_tolerance = 0.2;
-    orien_const.weight = 1.0;
+#     with open('/home/cc/ee106a/fl21/class/ee106a-aak/final/src/planning/src/trajectory.txt') as trajectory_txt:
+#         lines = trajectory_txt.readlines()
+#         lines = [line.rstrip() for line in lines]
 
-    orien_const_2 = PositionConstraint()
-    orien_const_2.link_name = "right_gripper_tip";
-    orien_const_2.header.frame_id = "base";
-    orien_const_2.target_point_offset.x = 0.6;
-    orien_const_2.target_point_offset.y = -0.3;
-    orien_const_2.target_point_offset.z = 0.0;
-    orien_const_2.weight = 1.0;
+#         trans_list = []
+#         quat_list = []
+
+#         for line in lines:
+#             if line.startswith("- Translation: "):
+#                 trans = line.split('[')[1][:-1]
+#                 trans = trans.split(', ')
+#                 trans_list.append(trans)
+
+#             if line.startswith("- Rotation: in Quaternion "):
+#                 quat = line.split('[')[1][:-1]
+#                 quat = quat.split(', ')
+#                 quat_list.append(quat)
+
+#         for i, (trans, quat) in enumerate(zip(trans_list, quat_list)):
+#             print("Moving to: trans " + str(trans) + ", quat " + str(quat))
+#             if i == 1:
+
+#             try:
+#                 #x, y, and z position
+#                 request.ik_request.pose_stamped.pose.position.x = float(trans[0])
+#                 request.ik_request.pose_stamped.pose.position.y = float(trans[1])
+#                 request.ik_request.pose_stamped.pose.position.z = float(trans[2])
+
+#                 x,y,z,w = float(quat[0]), float(quat[1]), float(quat[2]), float(quat[3])
+#                 norm = (x ** 2 + y ** 2 + z ** 2 + w ** 2) ** 0.5
+#                 x,y,z,w = x/norm, y/norm, z/norm, w/norm
+#                 print(x, y, z, w)
+
+#                 #Orientation as a quaternion
+#                 request.ik_request.pose_stamped.pose.orientation.x = x
+#                 request.ik_request.pose_stamped.pose.orientation.y = y
+#                 request.ik_request.pose_stamped.pose.orientation.z = z
+#                 request.ik_request.pose_stamped.pose.orientation.w = w
+
+#                 # Send the request to the service
+#                 response = compute_ik(request)
+                
+#                 # Print the response HERE
+#                 print(response)
+#                 group = MoveGroupCommander(arm + "_arm")
+
+#                 # Setting position and orientation target
+#                 group.set_pose_target(request.ik_request.pose_stamped)
+
+#                 # TRY THIS
+#                 # Setting just the position without specifying the orientation
+#                 ###group.set_position_target([0.5, 0.5, 0.0])
+
+#                 # Plan IK and execute
+#                 group.go()
+                
+#             except rospy.ServiceException, e:
+#                 print "Service call failed: %s"%e
+#                 sys.exit()
+
+#             # Close the right gripper
+#             rospy.sleep(0.1)
+#             if i == 1:
+#                 print('Closing...')
+#                 right_gripper.close()
+#                 rospy.sleep(1.0)
 
 
-    with open('/home/cc/ee106a/fl21/class/ee106a-aak/final/src/planning/src/trajectory.txt') as trajectory_txt:
-        lines = trajectory_txt.readlines()
-        lines = [line.rstrip() for line in lines]
+# # Python's syntax for a main() method
+# if __name__ == '__main__':
+#     main(sys.argv[1])
 
-        trans_list = []
-        quat_list = []
 
-        for line in lines:
-            if line.startswith("- Translation: "):
-                trans = line.split('[')[1][:-1]
-                trans = trans.split(', ')
-                trans_list.append(trans)
 
-            if line.startswith("- Rotation: in Quaternion "):
-                quat = line.split('[')[1][:-1]
-                quat = quat.split(', ')
-                quat_list.append(quat)
-
-        for trans, quat in zip(trans_list, quat_list):
-            while not rospy.is_shutdown():
-                try:
-                    goal_2 = PoseStamped()
-                    goal_2.header.frame_id = "base"
-
-                    # import pdb; pdb.set_trace()
-
-                    #x, y, and z position
-                    goal_2.pose.position.x = float(trans[0])
-                    goal_2.pose.position.y = float(trans[1])
-                    goal_2.pose.position.z = float(trans[2])
-
-                    #Orientation as a quaternion
-                    goal_2.pose.orientation.x = float(quat[0])
-                    goal_2.pose.orientation.y = float(quat[1])
-                    goal_2.pose.orientation.z = float(quat[2])
-                    goal_2.pose.orientation.w = float(quat[3])
-
-                    plan = planner.plan_to_pose(goal_2, [], [])
-
-                    raw_input("Press <Enter> to move the right arm to goal pose 3: ")
-                    if not planner.execute_plan(plan):
-                        raise Exception("Execution failed")
-                except Exception as e:
-                    print e
-                else:
-                    break
-
+#!/usr/bin/env python
+# The line above tells Linux that this file is a Python script, and that the OS
+# should use the Python interpreter in /usr/bin/env to run it. Don't forget to
+# use "chmod +x [filename]" to make this script executable.
+ 
+# Import the dependencies as described in example_pub.py
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+ 
+# Define the callback method which is called whenever this node receives a 
+# message on its subscribed topic. The received message is passed as the first
+# argument to callback().
+def callback(message):
+ 
+    # Print the contents of the message to the console
+    # print(rospy.get_name() + ": I heard \"%s\"" % message.data)
+    bridge = CvBridge()
+    cv_image = bridge.imgmsg_to_cv2(message, desired_encoding='passthrough')
+    print(cv_image)
+    print(cv_image.shape)
+ 
+# Define the method which contains the node's main functionality
+def listener():
+ 
+    # Create a new instance of the rospy.Subscriber object which we can use to
+    # receive messages of type std_msgs/String from the topic /chatter_talk.
+    # Whenever a new message is received, the method callback() will be called
+    # with the received message as its first argument.
+    rospy.Subscriber("camera/depth/image_rect_raw", Image, callback)
+ 
+    # Wait for messages to arrive on the subscribed topics, and exit the node
+    # when it is killed with Ctrl+C
+    rospy.spin()
+ 
+ 
+# Python's syntax for a main() method
 if __name__ == '__main__':
-    rospy.init_node('moveit_node')
-    main()
+ 
+    # Run this program as a new node in the ROS computation graph called
+    # /listener_<id>, where <id> is a randomly generated numeric string. This
+    # randomly generated name means we can start multiple copies of this node
+    # without having multiple nodes with the same name, which ROS doesn't allow.
+    rospy.init_node('listener', anonymous=True)
+ 
+    listener()
